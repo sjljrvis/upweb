f303907a208cd1b549c22e72475d7472a480cc3a
sjljrvis <sjlchougule@gmail.com>
2019-09-09 02:59:12 +0530

    Added model hooks

--- models/repository.go
+++ models/repository.go
@@ -1,23 +1,51 @@
 package models
 
 import (
-	// "log"
-	// "github.com/jinzhu/gorm"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	fs "github.com/sjljrvis/deploynow/lib/fs"
+	git "github.com/sjljrvis/deploynow/lib/git"
 )
- 
+
 type Repository struct {
 	Base
-	RepositoryName string                 `gorm:"unique" json:"repository_name"`
-	Language       string                 `json:"language"`
-	Path           string                 `json:"path"`
-	PathDocker     string                 `json:"path_docker"`
-	Description    string                 `json:"description"`
-	State          string                 `json:"state"`
+	RepositoryName string `gorm:"unique" json:"repository_name"`
+	Language       string `json:"language"`
+	Path           string `json:"path"`
+	PathDocker     string `json:"path_docker"`
+	Description    string `json:"description"`
+	State          string `json:"state" default:"stopped"`
+	UserID         uint   `json:"user_id"`
+	UserName       string `json::"user_name"`
 }
- 
+
 // BeforeCreate will set a UUID rather than numeric ID.
-// func (base *Repo) BeforeCreate(scope *gorm.Scope) error {
-// 	log.Printf("I am here")
-// 	return nil
-//  }
+func (repo *Repository) BeforeCreate(scope *gorm.Scope) (err error) {
+	repositoryPath := path.Join(os.Getenv("ROOT_DIR"), repo.UserName, repo.RepositoryName)
+	repositoryPathDocker := path.Join(os.Getenv("ROOT_DIR"), repo.UserName, repo.RepositoryName+"_docker")
+	scope.SetColumn("Path", repositoryPath)
+	scope.SetColumn("PathDocker", repositoryPathDocker)
+	return
+}
+
+// AfterCreate will set a UUID rather than numeric ID.
+/*
+	//TODO
+	1) Create bare repository
+	2) Add hook files
+	3) Lauch default container
+*/
+func (repo *Repository) AfterCreate(scope *gorm.Scope) (err error) {
+	err = fs.CreateDir(repo.Path)
+	err = fs.CreateDir(repo.PathDocker)
+	err = git.InitBare(repo.Path)
+	err = git.CreateHooks(repo.Path)
+	if err != nil {
+		fmt.Printf("Error Occured")
+	}
+	return scope.SetColumn("State", "stopped")
+}
--- models/user.go
+++ models/user.go
@@ -2,19 +2,20 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	Helper "github.com/sjljrvis/deploynow/helpers"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	Helper "github.com/sjljrvis/deploynow/helpers"
 )
- 
+
 type User struct {
 	Base
-	UserName string                 `gorm:"unique" json:"user_name"`
-	Email    string                 `gorm:"unique" json:"email"`
-	Password string                 `json:"password"`
+	UserName     string `gorm:"unique" json:"user_name"`
+	Email        string `gorm:"unique" json:"email"`
+	Password     string `json:"password"`
+	Repositories Repository
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
 func (user *User) BeforeSave(scope *gorm.Scope) error {
 	hashed, _ := Helper.HashPassword(user.Password)
 	return scope.SetColumn("Password", hashed)
- }
+}
