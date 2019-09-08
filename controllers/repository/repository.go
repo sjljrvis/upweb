64ff1b593cbacf0db58b65891c99e74587b76997
sjljrvis <sjlchougule@gmail.com>
2019-09-09 02:59:54 +0530

    added auth middleware

--- controllers/repository/repository.go
+++ controllers/repository/repository.go
@@ -3,25 +3,31 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
-// "log"
+
+	// "log"
 	"github.com/gorilla/mux"
+	. "github.com/sjljrvis/deploynow/db"
 	Helper "github.com/sjljrvis/deploynow/helpers"
 	models "github.com/sjljrvis/deploynow/models"
-	."github.com/sjljrvis/deploynow/db" 
 )
 
 // GetAll controller
 func GetAll(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := ctx.Value("user").(models.User)
 	repositories := []models.Repository{}
-	DB.Find(&repositories)
+	DB.Find(&user).Related(&repositories)
 	Helper.RespondWithJSON(w, 200, repositories)
 }
 
 //Get controller
 func Get(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := ctx.Value("user").(models.User)
 	params := mux.Vars(r)
 	repository := models.Repository{}
-	if err := DB.First(&repository,params["id"]).Error; err != nil {
+	err := DB.Where("user_id = ?", user.ID).First(&repository, params["id"]).Error
+	if err != nil {
 		Helper.RespondWithError(w, http.StatusNotFound, err.Error())
 		return
 	}
@@ -29,6 +35,8 @@ func Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := ctx.Value("user").(models.User)
 	repository := models.Repository{}
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&repository); err != nil {
@@ -36,6 +44,8 @@ func Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer r.Body.Close()
+	repository.UserID = user.ID
+	repository.UserName = user.UserName
 	if err := DB.Save(&repository).Error; err != nil {
 		Helper.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
@@ -46,31 +56,31 @@ func Create(w http.ResponseWriter, r *http.Request) {
 func Update(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	repository := models.Repository{}
-	if err := DB.First(&repository,params["id"]).Error; err != nil {
+	if err := DB.First(&repository, params["id"]).Error; err != nil {
 		Helper.RespondWithError(w, http.StatusNotFound, err.Error())
 		return
-	} 
+	}
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&repository); err != nil {
 		Helper.RespondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer r.Body.Close()
- 
+
 	if err := DB.Save(&repository).Error; err != nil {
 		Helper.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 	Helper.RespondWithJSON(w, http.StatusOK, repository)
 }
- 
+
 func Delete(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	repository := models.Repository{}
-	if err := DB.First(&repository,params["id"]).Error; err != nil {
+	if err := DB.First(&repository, params["id"]).Error; err != nil {
 		Helper.RespondWithError(w, http.StatusNotFound, err.Error())
 		return
-	}  
+	}
 	if err := DB.Delete(&repository).Error; err != nil {
 		Helper.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
